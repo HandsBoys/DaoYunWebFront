94cb6abc2ec6369b5ccb6adb0b52ad308d965360
CCH-sudo <60307397+CCH-sudo@users.noreply.github.com>
2021-03-27 22:50:14 +0800

    1

--- daoyunwebfront/src/main.js
+++ daoyunwebfront/src/main.js
@@ -3,16 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import Element from 'element-ui'
+import Cookies from 'js-cookie'
+import './assets/styles/element-variables.scss'
+import store from './store'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI);
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  render: h => h(App)
 })
---
+++ 新建文件夹 (2)/新建文件夹 (2)/daoyunwebfront/src/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+<h1>ddddddd</h1>
+<svg-icon icon-class="404" />
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "index"
+};
+</script>
+
+<style>
+</style>
